d73fbcc6cb6b33a65b28a503f653c1648f13a383
AAMasud <amasud183040@bscse.uiu.ac.bd>
2023-04-10 23:19:36 +0600

    Initial Node JS server

---
+++ app.js
@@ -0,0 +1,22 @@
+// Import required modules
+const express = require('express');
+const path = require('path');
+
+// Create an instance of Express app
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from "public" folder
+
+// Send HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
